489a5d73daa13df9ba043a24709229f72b8a8cfe
LaceyBaughman <laceyebaughman@gmail.com>
2022-03-02 14:08:08 -0700

    env postman setup

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'http://localhost:3000' : 'http://localhost:8080'
+export const domain = 'dev-yazlhsae.us.auth0.com'
+export const audience = 'https://lacey-dev.com'
+export const clientId = '10HXKSqllHvfYu0GKY8Rs89FfxGzs23q'
